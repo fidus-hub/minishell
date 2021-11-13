93569e3b5aa4cb163ca7da79369f95df60e7d3c8
Mohamed amine Grissen <mgrissen@e2r1p11.1337.ma>
2021-11-13 12:05:57 +0100

    working on export

--- builtins/env.c
+++ builtins/env.c
@@ -2,12 +2,32 @@
 
 int	env(t_env *env_h)
 {
-    if (!env_h)
+    t_env *tmp;
+
+    tmp = env_h;
+    if (!tmp)
         return (-1);
-    while (env_h)
+    while (tmp)
     {
-        printf("%s=%s\n", env_h->var, env_h->val);
-        env_h = env_h->suivant;
+        printf("%s=%s\n", tmp->var, tmp->val);
+        tmp = tmp->suivant;
     }
+    t_env*newnode = new_node(args[1], args[2]);
+    tmp->suivant = newnode;
+    newnode->preced = tmp;
     return (0);
+
+
+}
+
+t_env   *new_node(char *var, char *val)
+{
+    t_env   *new;
+
+    new = malloc(sizeof(t_env));
+    new->var = var;
+    new->val = val;
+    new->preced = NULL;
+    new->suivant = NULL;
+    return new;
 }
